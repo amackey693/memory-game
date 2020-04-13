2f023cf3868dbe9f1c1bbcf8714203ab196963f0
Allison Mackey <a.mackey693@gmail.com>
2020-04-13 15:12:53 -0700

    add values to cards in html, styling to css, if/if else statements to main.js.

--- src/main.js
+++ src/main.js
@@ -1,31 +1,34 @@
-// User Interface Logic -->
+// // User Interface Logic -->
 import { CardSet } from './memory';
-import { matchingCard } from './memory';
+// import matchingCard from './memory';
 import './styles.css';
+import $ from 'jquery';
 
 //event listeners functions!
 
 // var card1 = $(this).val(); --> "7"
 // var card2 = $(this).val(); --> "9"
 
-$ (document).ready(function() {
+$(document).ready(function() {
+
   //event listeners here
-  $(".cards").addEventListener('click', function(){
-    (this).children("p").slideToggle();
+  $(".cards").click(function(){
+    (this).removeClass(".cards");
     var card1 = $(this).val(); 
     var card2 = $(this).val();
     var setOne = new CardSet (card1,card2);
     var matching = setOne.matchingCard();
+    
+    if (setOne.show.length === 2) {
+      (this).addClass(".matching")
+    } else if (setOne.show.length === 0) {
+      (this).addClass(".cards")
+    }
 
     // output 
-    $("#").html(matching);  
+    $("#output").html(matching);  
   });
-}):
-
-
-
-
-
+});
 
 // $ (document).ready(function() {
 //   //event listeners here
@@ -40,3 +43,14 @@ $ (document).ready(function() {
 //     $("#").html(matching);  
 //   });
 // }):
+
+// var gameMatrix = [card1, card2, card3, card4]
+  // gameMatrix.forEach(function(thing){
+  //   if(thing has value)
+  //   $("#" + thing).html("<div class="col">
+  //       <div class="set-one cards">
+  //         <!-- <p><img src="/img/backside.png" alt="back of card"></p>
+  //         <p><img src="/img/ace.png" alt="front-ace"></p> -->
+  //       </div>
+  //     </div>")
+  // })
--- src/memory.js
+++ src/memory.js
@@ -1,27 +1,39 @@
 // Business Logic -->
 // for Card Set Constructor : 
-function CardSet (card1, card2) {
+
+export function CardSet (card1, card2) {
   this.card1 = card1;
   this.card2 = card2;
   this.show = [];
-}
+};
 
 CardSet.prototype.matchingCard = function () {
   if (this.card1 !== this.card2) {
     this.show = [];
   } else if (this.card1 === this.card2)  {
     this.show.push(this.card1 + this.card2);
-  }
+  } 
   return this.show;
 };
 
+// CardSet.prototype.pairCheck = function () {
+//   if (this.show.length > 2) {
+//   this.show.push(this.card2);
+// } else if (this.show.length === 2) {
+//   this.show.matchingCard();
+// }
+
+CardSet.prototype.matchingCard = function () {
+  if (this.show[0] === this.show[1])
+    do something cool
+  } else {
+    flip both cards back over
+  }
+}  
 // - if items clicked on then --> run method
 // - add event listeners to 
- 
 
-
-
-var set1 = new CardSet (card1, card2)
+// var set1 = new CardSet (card1, card2)
 // var matching = set1.matchingCard();
 // console.log(matching)
 // console.log(set1);
